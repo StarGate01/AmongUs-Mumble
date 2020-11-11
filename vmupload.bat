536c9935b5f2557add9d6a6d00fc365f4d498eda
Christoph Honal <christoph.honal@web.de>
2020-11-11 16:22:05 +0100

    Fix the batch scripts

--- vmupload.bat
+++ vmupload.bat
@@ -1,3 +1,14 @@
-@echo off
-gamepath.bat
-copy /b /v /y ".\Release\winhttp.dll" "%AMONGUS%\winhttp.dll"
+@ECHO OFF
+
+CD /D "%~dp0"
+CALL setup.bat
+
+ECHO "Copying to %AMONGUS%"
+COPY /b /v /y ".\Release\winhttp.dll" "%AMONGUS%\winhttp.dll"
+
+IF DEFINED VM_UPLOAD (
+    FOR %%x IN (%VM_SUFFIXES%) DO (
+        ECHO "Copying to VM %VM_BASENAME%%%x"
+        COPY /b /v /y ".\Release\winhttp.dll" "\\%VM_BASENAME%%%x\%VM_AMONGUS%\winhttp.dll"
+    )
+)

fe4e583a203102972be7fae8a400a2b4df6f58aa
Christoph Honal <christoph.honal@web.de>
2020-11-22 01:21:13 +0100

    Begin researching beta deobfuscations

--- AUMInjector/user/deobfuscate.h
+++ AUMInjector/user/deobfuscate.h
@@ -37,7 +37,7 @@ using namespace app;
 	#define MeetingHud_Close_Trampoline GPOHFPAIEMA_Close
 	#define MeetingHud_Start_Trampoline GPOHFPAIEMA_Start
 	#define InnerNetClient_FixedUpdate_Trampoline DNAFMCDBMCI_FixedUpdate
-	#define InnerNetClient_Disconnect_Trampoline DNAFMCDBMCI_GPKBCOKMFHK //DNAFMCDBMCI_HandleDisconnect ? 
+	#define InnerNetClient_Disconnect_Trampoline DNAFMCDBMCI_HandleDisconnect
 	#define HqHudOverrideTask_Initialize_Trampoline MMPIJGKLMDC_Initialize
 	#define HqHudOverrideTask_Complete_Trampoline MMPIJGKLMDC_Complete
 	#define HudOverrideTask_Initialize_Trampoline GFBKIHICFNI_Initialize
@@ -66,20 +66,32 @@ using namespace app;
 	//#define HudOverrideSystemType_Deserialize_Trampoline HHIJNMBJCAE_Deserialize
 	//#define HudOverrideSystemType_RepairDamage_Trampoline HHIJNMBJCAE_RepairDamage
 #elif GAME_VERSION == GAME_VERSION_2020_11_4s
-	//TODO add new functions
 	using InnerNetClient_GameState__Enum = DBDDAJAICFN_DKEKFCCGGEO__Enum;
 	using PlayerControl = APNNOJFGDGP;
 	using Player_Die_Reason__Enum = LKBAAGPFMCB__Enum;
-	using MeetingHud = LFBAPIAFCFM;
+	using MeetingHud = APFHIONIINA;
 	using InnerNetClient = DBDDAJAICFN;
+	using InnerNet_DisconnectReasons__Enum = OBELNHOKKLD__Enum;
+	using HqHudOverrideTask = OFCAMODPCOA;
+	using HudOverrideTask = MHKPCHGHHOK;
+	using ChatController = LOGKEMANPPL;
 	InnerNetClient_GameState__Enum InnerNetClient_GameState__Enum_Joined = DBDDAJAICFN_DKEKFCCGGEO__Enum_Joined;
 	InnerNetClient_GameState__Enum InnerNetClient_GameState__Enum_Ended = DBDDAJAICFN_DKEKFCCGGEO__Enum_Ended;
+	InnerNetClient_GameState__Enum InnerNetClient_GameState__Enum_Started = DBDDAJAICFN_DKEKFCCGGEO__Enum_Started;
 	#define PlayerControl_FixedUpdate_Trampoline APNNOJFGDGP_FixedUpdate
 	#define PlayerControl_GetTruePosition_Trampoline APNNOJFGDGP_GetTruePosition
 	#define PlayerControl_Die_Trampoline APNNOJFGDGP_Die
-	#define MeetingHud_Close_Trampoline LFBAPIAFCFM_Close
-	#define MeetingHud_Start_Trampoline LFBAPIAFCFM_Start
+	#define MeetingHud_Close_Trampoline APFHIONIINA_Close
+	#define MeetingHud_Start_Trampoline APFHIONIINA_Start
 	#define InnerNetClient_FixedUpdate_Trampoline DBDDAJAICFN_FixedUpdate
+	#define InnerNetClient_Disconnect_Trampoline DBDDAJAICFN_HandleDisconnect
+	#define HqHudOverrideTask_Initialize_Trampoline OFCAMODPCOA_Initialize
+	#define HqHudOverrideTask_Complete_Trampoline OFCAMODPCOA_Complete
+	#define HudOverrideTask_Initialize_Trampoline MHKPCHGHHOK_Initialize
+	#define HudOverrideTask_Complete_Trampoline MHKPCHGHHOK_Complete
+	#define ChatController_AddChat_Trampoline LOGKEMANPPL_AddChat
+	#define InnerNetClient_StartRpc DBDDAJAICFN_StartRpc
+	#define MessageWriter_Write_String MessageWriter_Write_6
 #else
 	#error Unknown game version!
 #endif

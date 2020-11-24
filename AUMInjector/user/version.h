06a01df27146b507cc4a50748bf0925f800b6a66
Christoph Honal <christoph.honal@web.de>
2020-11-24 02:22:30 +0100

    new version deobfuscated

--- AUMInjector/user/version.h
+++ AUMInjector/user/version.h
@@ -1,8 +1,9 @@
 #define GAME_VERSION_2020_9_22s 20209220
 #define GAME_VERSION_2020_10_22s 202010220
 #define GAME_VERSION_2020_11_4s 202011040
+#define GAME_VERSION_2020_11_17s 202011170
 #ifndef GAME_VERSION
-	#define GAME_VERSION GAME_VERSION_2020_10_22s
+	#define GAME_VERSION GAME_VERSION_2020_11_17s
 #endif
 
 #if GAME_VERSION == GAME_VERSION_2020_9_22s
@@ -11,6 +12,8 @@
 	#define version_text "2020.10.22s"
 #elif GAME_VERSION == GAME_VERSION_2020_11_4s
 	#define version_text "2020.11.4s"
+#elif GAME_VERSION == GAME_VERSION_2020_11_17s
+	#define version_text "2020.11.17s"
 #else
 	#error Unknown game version!
 #endif
